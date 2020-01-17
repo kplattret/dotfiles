729e477138fa19a8796dd13c62e87e5e6283f7c9
Kevin Plattret <kevin@plattret.com>
2020-01-17 19:58:00 +0000

    Move ZSH aliases to sepparate file and add some
    Moves existing ZSH aliases to a separate file so that they don't clutter
    the main config file. Adds all the Rails aliases back as I still need
    them and I'm not sure why I got rid of them in the first place.

---
+++ zsh/zsh-aliases.sh
@@ -0,0 +1,16 @@
+# Package manager aliases
+alias brewup="brew update && brew upgrade && brew doctor"
+alias aptup="sudo apt update && sudo apt upgrade"
+
+# Git custom aliases (in addition to plugin's ones)
+alias gsf="gco staging && git reset --hard origin/master && gm - && gpf"
+
+# Rails aliases
+alias rails="bundle exec rails"
+alias rdbc="rails db:create"
+alias rdbd="rails db:drop"
+alias rdbdc="rails db:drop && rails db:create"
+alias rdbm="rails db:migrate && rails db:migrate RAILS_ENV=test"
+alias rdbrb="rails db:rollback && rails db:rollback RAILS_ENV=test"
+alias rdbs="rails db:seed && rails db:seed RAILS_ENV=test"
+alias rspec="bundle exec rspec"
