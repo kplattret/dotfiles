547f9cb5882dc7b3b51e7723927823d384f3c9bd
Kevin Plattret <kevin@plattret.com>
2024-04-25 00:17:47 +0200

    Move git config files to ~/.config/git/ folder

--- install.sh
+++ install.sh
@@ -96,20 +96,30 @@ function setup_dotfiles() {
 
   mkdir -p ~/.config/alacritty
   ln -s ~/Code/kplattret/dotfiles/alacritty/alacritty.toml ~/.config/alacritty/alacritty.toml
+
   ln -s ~/Code/kplattret/dotfiles/asdf/asdfrc ~/.asdfrc
+
   mkdir ~/.cargo
   ln -s ~/Code/kplattret/dotfiles/cargo/config.toml ~/.cargo/config.toml
-  ln -s ~/Code/kplattret/dotfiles/git/gitconfig ~/.gitconfig
-  ln -s ~/Code/kplattret/dotfiles/git/gitignore_global ~/.gitignore_global
+
+  mkdir ~/.config/git
+  ln -s ~/Code/kplattret/dotfiles/git/config ~/.config/git/config
+  ln -s ~/Code/kplattret/dotfiles/git/ignore ~/.config/git/ignore
+
   mkdir ~/.gnupg
   ln -s ~/Code/kplattret/dotfiles/gnupg/gpg-agent.conf ~/.gnupg/gpg-agent.conf
   ln -s ~/Code/kplattret/dotfiles/gnupg/gpg.conf ~/.gnupg/gpg.conf
+
   ln -s ~/Code/kplattret/dotfiles/nvim ~/.config/nvim/lua/user
+
   mkdir ~/.ssh
   ln -s ~/Code/kplattret/dotfiles/ssh/config ~/.ssh/config
+
   ln -s ~/Code/kplattret/dotfiles/tmux/tmux.conf ~/.tmux.conf
+
   ln -s ~/Code/kplattret/dotfiles/vim ~/.vim
   ln -s ~/Code/kplattret/dotfiles/vim/vimrc ~/.vimrc
+
   mkdir ~/.config/zsh
   ln -s ~/Code/kplattret/dotfiles/zsh/zshenv ~/.zshenv
   ln -s ~/Code/kplattret/dotfiles/zsh/zshrc ~/.config/zsh/.zshrc
