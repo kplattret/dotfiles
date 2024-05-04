df16b7ba818ffca3cf8b8004bd48caf52412f4fb
Kevin Plattret <kevin@plattret.com>
2024-05-04 14:57:53 +0100

    Add AstroNvim's Rust language pack

---
+++ nvim/lua/plugins/astrolsp.lua
@@ -0,0 +1,23 @@
+-- AstroLSP allows you to customize the features in AstroNvim's LSP configuration engine
+-- Configuration documentation can be found with `:h astrolsp`
+
+---@type LazySpec
+return {
+  "AstroNvim/astrolsp",
+  ---@type AstroLSPOpts
+  config = {
+    rust_analyzer = {
+      settings = {
+        ["rust-analyzer"] = {
+          cargo = {
+            -- Make rust-analyzer use its own profile
+            -- Uses more disk space as it will create a separate folder in `targets`
+            -- Allows to run `cargo build` without being blocked
+            extraEnv = { CARGO_PROFILE_RUST_ANALYZER_INHERITS = "dev" },
+            extraArgs = { "--profile", "rust-analyzer" },
+          },
+        },
+      },
+    },
+  },
+}
