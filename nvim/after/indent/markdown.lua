22e7e271813b06789d56907fa2fb4c5bd73d62c2
Kevin Plattret <kevin@plattret.com>
2024-06-19 11:05:20 +0100

    Set Markdown specific settings in Neovim

---
+++ nvim/after/indent/markdown.lua
@@ -0,0 +1,12 @@
+local opt = vim.opt_local
+
+-- opt.spell = true
+opt.expandtab = true
+opt.autoindent = true
+opt.smarttab = true
+opt.shiftwidth = 4
+opt.tabstop = 4
+opt.softtabstop = 4
+-- opt.breakindentopt = "list:4"
+
+-- print "markdown"
